673c1f5584637c8a151a1734c7b3dbdbd2114b11
Walter N <2760walter@gmail.com>
2023-10-06 22:53:23 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+const errMessage = document.getElementById('modal-message');
 
 
 
@@ -20,6 +22,44 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
-  });
+    }, 300);  
+  })
+  
 }
+
+
+
+
+
+
+const hearts = document.querySelectorAll('.like-glyph');
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', (event) => {
+      mimicServerCall()
+      .then(() => {
+        console.log(event.target);
+        if (event.target.classList.contains("activated-heart")) {
+          event.target.classList.remove("activated-heart");
+          console.log( 'before if');
+          event.target.innerText = EMPTY_HEART;
+        } else {
+          event.target.classList.add("activated-heart");
+          event.target.innerText = FULL_HEART;
+          console.log( 'after if');
+        }
+      })
+      .catch(() => {
+        errorModal.classList.remove("hidden");
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+    }
+  
+  
+   
+  )
+});
+
+
